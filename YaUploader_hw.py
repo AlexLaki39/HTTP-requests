712520a82bdf65b46c1390881a3318557da03571
Aleksandr Lakienko <sasha-lakienko5@mail.ru>
2022-11-12 13:04:26 +0300

    add task 2

---
+++ YaUploader_hw.py
@@ -0,0 +1,32 @@
+import requests
+
+
+class YaUploader:
+    base_host = 'https://cloud-api.yandex.net:443/'
+
+    def __init__(self, token: str):
+        self.token = token
+
+    def get_headers(self):
+        return {
+            'Content-type': 'application/json',
+            'Authorization': f'OAuth {self.token}'
+        }
+
+    def upload(self, file_path: str):
+        uri = 'v1/disk/resources/upload/'
+        requests_url = self.base_host + uri
+        params = {'path': file_path, 'overwrite': True}
+        response = requests.get(requests_url, headers=self.get_headers(),
+                                params=params)
+        response1 = requests.put(response.json()['href'],
+                                 data=open(file_path, 'rb'),
+                                 headers=self.get_headers())
+        print(response1.status_code)
+
+
+if __name__ == '__main__':
+    path_to_file =
+    token =
+    ya = YaUploader(token)
+    result = ya.upload(path_to_file)
--- superhero_hw.py
+++ superhero_hw.py
@@ -1,37 +1,66 @@
 import requests
 
 
-class SuperHeroApi:
-    base_url = 'https://akabab.github.io/superhero-api/api/'
-    our_superheroes = []
-    superheroes_intelligence = {}
+base_url = 'https://akabab.github.io/superhero-api/api/'
+heroes_list = ['Hulk', 'Captain America', 'Thanos']
+superheroes_intelligence = {}
+uri_all_json = '/all.json'
 
-    def __init__(self, name):
-        self.name = name
-        if name not in SuperHeroApi.our_superheroes:
-            SuperHeroApi.our_superheroes.append(name)
-        self._get_heroes_intelligence()
+requests_url = base_url + uri_all_json
+response = requests.get(requests_url)
 
-    def _get_heroes_intelligence(self):
-        uri_all_json = '/all.json'
-        response = requests.get(SuperHeroApi.base_url + uri_all_json)
-        list_all_heroes = response.json()
-        for items in list_all_heroes:
-            if items['name'] in SuperHeroApi.our_superheroes:
-                SuperHeroApi.superheroes_intelligence[items['name']] = \
-                    items['powerstats']['intelligence']
+for items in response.json():
+    if items['name'] in heroes_list:
+        superheroes_intelligence[items['name']] = \
+            items['powerstats']['intelligence']
+most_intell = max(superheroes_intelligence)
+print(f'Самым умным среди {",".join(superheroes_intelligence)} '
+    f'оказался {most_intell}')
 
 
-def most_intelligent_superhero():
-    most_intell = max(SuperHeroApi.superheroes_intelligence)
-    print(
-        f'Самым умным среди {",".join(SuperHeroApi.our_superheroes)} '
-        f'оказался {most_intell}')
+## Второй вариант хотел попробовать с классами, хотя они тут и не нужны
+## И ту вопрос можно ли в методе init вызвать другой метод который ниже?(правильно ли так делать?)
 
 
-Thanos = SuperHeroApi('Thanos')
-Hulk = SuperHeroApi('Hulk')
-Captain_America = SuperHeroApi('Captain America')
-
-most_intelligent_superhero()
+# class SuperHeroApi:
+#     base_url = 'https://akabab.github.io/superhero-api/api/'
+#     our_superheroes = []
+#     superheroes_intelligence = {}
+#
+#     def __init__(self, name: str):
+#         self.name = name
+#         if name not in SuperHeroApi.our_superheroes:
+#             SuperHeroApi.our_superheroes.append(name)
+#         self._get_heroes_intelligence()
+#
+#     def _get_heroes_intelligence(self):
+#         '''
+#         Получает значение ителлекта героя
+#         '''
+#         uri_all_json = '/all.json'
+#         response = requests.get(SuperHeroApi.base_url + uri_all_json)
+#         list_all_heroes = response.json()
+#         for items in list_all_heroes:
+#             if items['name'] in SuperHeroApi.our_superheroes:
+#                 SuperHeroApi.superheroes_intelligence[items['name']] = \
+#                     items['powerstats']['intelligence']
+#             elif items['name'] in SuperHeroApi.our_superheroes:
+#                 break
+#
+#
+# def most_intelligent_superhero():
+#     '''
+#     Выводит самого умного героя
+#     '''
+#     most_intell = max(SuperHeroApi.superheroes_intelligence)
+#     print(
+#         f'Самым умным среди {",".join(SuperHeroApi.our_superheroes)} '
+#         f'оказался {most_intell}')
+#
+#
+# Hulk = SuperHeroApi('Hulk')
+# Thanos = SuperHeroApi('Thanos')
+# Captain_America = SuperHeroApi('Captain America')
+#
+# most_intelligent_superhero()
 
