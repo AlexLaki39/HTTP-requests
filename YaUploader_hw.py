2709454c61cfdae25b559f7ac733bc9ef8258c11
Aleksandr Lakienko <sasha-lakienko5@mail.ru>
2022-11-19 12:55:26 +0300

    вернули открытие файла в загрузке на ядиск

--- YaUploader_hw.py
+++ YaUploader_hw.py
@@ -20,12 +20,13 @@ class YaUploader:
         response = requests.get(requests_url, headers=self.get_headers(),
                                 params=params)
         response1 = requests.put(response.json()['href'],
-                                 data=file_path)
+                                 data=open(file_path, 'rb'),
+                                 headers=self.get_headers())
         print(response1.status_code)
 
 
 if __name__ == '__main__':
-    path_to_file =
-    token =
+    path_to_file = ' '
+    token = ' '
     ya = YaUploader(token)
     result = ya.upload(path_to_file)
